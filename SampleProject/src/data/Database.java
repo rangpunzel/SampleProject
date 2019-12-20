7047025ebe1865bac613c732d07c2625b26b55a8
rangpunzel <57884028+rangpunzel@users.noreply.github.com>
2019-12-20 19:35:35 +0900

    Add files via upload

---
+++ SampleProject/src/data/Database.java
@@ -0,0 +1,30 @@
+package data;
+
+import java.util.ArrayList;
+
+import vo.UserVO;
+
+public class Database {
+	
+private static Database instance;
+	
+	private Database(){}
+	
+	public static Database getInstance(){
+		if(instance == null){
+			instance = new Database();
+		}
+		return instance;
+	}
+	
+	public ArrayList<UserVO> tb_user = new ArrayList<>();
+
+	{
+		UserVO user = new UserVO();
+		user.setId("admin");
+		user.setPassword("admin");
+		user.setName("관리자");
+		tb_user.add(user);
+	}
+	
+}
---
+++ SampleProject/src/data/Session.java
@@ -0,0 +1,10 @@
+package data;
+
+import vo.UserVO;
+
+//세션 : 인터넷창이 열려있는 동안 유지되는 저장 공간
+public class Session {
+	//로그인 되어있는 유저의 정보를 저장
+	public static UserVO loginUser;
+
+}
